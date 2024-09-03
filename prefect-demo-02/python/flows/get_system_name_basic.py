e7132c037ff8c5dd3a7adbdafe29dc969b34dfee
Siva Nadesan <entechlog@gmail.com>
2024-09-02 22:07:55 -0700

    changes to add more example for prefect demo

---
+++ prefect-demo-02/python/flows/get_system_name_basic.py
@@ -0,0 +1,14 @@
+import socket
+
+def get_system_name():
+    """
+    Function to log the system's hostname.
+    """
+    # Get the system hostname
+    system_name = socket.gethostname()
+
+    # Print the message with the system name
+    print(f"Hello from {system_name}!!")
+
+if __name__ == "__main__":
+    get_system_name()
